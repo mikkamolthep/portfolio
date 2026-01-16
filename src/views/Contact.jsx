2a48c784adc6b1680510d4affc80fa8b134c64b1
mikkamolthep <mik.kamolthep@gmail.com>
2026-01-16 09:59:01 +0700

    edited imges

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -9,7 +9,7 @@ export default function Contact(){
             className="p-2 bg-amber-200 rounded-lg shadow flex items-center gap-2"
           >
             <img
-              src="GitHub.png"
+              src="/GitHub.png"
               alt="github-logo"
               className="w-[25px] h-[25px] object-cover"
             />
@@ -22,7 +22,7 @@ export default function Contact(){
             className="p-2 bg-amber-200 rounded-lg shadow flex items-center gap-2"
           >
             <img
-              src="LinkedIn.png"
+              src="/LinkedIn.png"
               alt="linkedin-logo"
               className="w-[25px] h-[25px] rounded-full object-cover"
             />
@@ -33,7 +33,7 @@ export default function Contact(){
             className="p-2 bg-amber-200 rounded-lg shadow flex items-center gap-2"
           >
             <img
-              src="Gmail-Logo.png"
+              src="/Gmail-Logo.png"
               alt="gmail-logo"
               className="w-[20px] h-[20px] object-cover"
             />
